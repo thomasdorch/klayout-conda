92ac420a87a9d584dfa58aa52d9ac5364a374e05
thomasdorch <thomaspdorch@gmail.com>
2022-12-04 17:34:07 -0800

    python3.11 works, 3.10 and 3.9 do not

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,36 +1,31 @@
 setlocal EnableDelayedExpansion
 
 set CONFIG=release
+set "out_dir=%LIBRARY_BIN%"
+
 set HAVE_QTBINDINGS=1
 set HAVE_QT=1
 set HAVE_64BIT_COORD=0
 set HAVE_PYTHON=1
 set HAVE_RUBY=1
-set MAKE_OPT=
 set HAVE_CURL=0
 set HAVE_EXPAT=0
 set HAVE_PTHREADS=0
 
-set "p=/"
-set "r=\"
-
-for /f "tokens=*" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('VERSION'))"') do (
-	set "PYVER=%%i"
-)
-for /f "tokens=*" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('installed_base'))"') do (
-	set "d=%%i"
-	set "PYBASE=!d:%p%=%r%!"
-)
-for /f "tokens=*" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('INCLUDEPY'))"') do (
-	set "d=%%i"
-	set "PYTHONINCLUDE=!d:%p%=%r%!"
-)
+rem Get Python paths
 for /f "tokens=*" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))"') do (
 	set "PYTHONEXTSUFFIX=%%i"
 )
+for /f "tokens=*" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('VERSION'))"') do (
+	set "PYTHONVERSION=%%i"
+)
+for /f "tokens=*" %%i in ('python -c "import sysconfig; print(sysconfig.get_config_var('INCLUDEPY'))"') do (
+	set "PYTHONINCLUDE=%%i"
+)
+rem Make sure paths have the correct slashes
+set "PYTHONINCLUDE=!PYTHONINCLUDE:/=\!"
 
-set "PYTHONLIBFILE=%PYBASE%\libs\python%PYVER%.lib"
-
+set "PYTHONLIBFILE=%PREFIX%\libs\python%PYTHONVERSION%.lib"
 
 echo "    Python installation is in:"
 echo "    - %PYTHONLIBFILE% 		(lib)"
@@ -39,14 +34,7 @@ echo "    - %PYTHONEXTSUFFIX% 	(ext. suffix)"
 echo ""
 
 
-for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['rubyhdrdir'] || '')"') do (
-	set "d=%%i"
-	set "RUBYINCLUDE=!d:%p%=%r%!"
-)
-for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['rubyarchhdrdir'] || '')"') do (
-	set "d=%%i"
-	set "RUBYINCLUDE2=!d:%p%=%r%!"
-)
+rem Get Ruby paths
 for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['MAJOR'] || 0).to_i*10000+(RbConfig::CONFIG['MINOR'] || 0).to_i*100+(RbConfig::CONFIG['TEENY'] || 0).to_i"') do (
 	set "RUBYVERSIONCODE=%%i"
 )
@@ -54,9 +42,19 @@ for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['LIBRUBY']
 	set "RUBYLIBFILENAME=%%i"
 )
 for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['libdir'] || '')"') do (
-	set "d=%%i"
-	set "RUBYLIBFILEPATH=!d:%p%=%r%!"
+	set "RUBYLIBFILEPATH=%%i"
 )
+for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['rubyhdrdir'] || '')"') do (
+	set "RUBYINCLUDE=%%i"
+)
+for /f "tokens=*" %%i in ('ruby -rrbconfig -e "puts (RbConfig::CONFIG['rubyarchhdrdir'] || '')"') do (
+	set "RUBYINCLUDE2=%%i"
+)
+rem Make sure paths have the correct slashes
+set "RUBYLIBFILEPATH=!RUBYLIBFILEPATH:/=\!"
+set "RUBYINCLUDE2=!RUBYINCLUDE2:/=\!"
+set "RUBYINCLUDE=!RUBYINCLUDE:/=\!"
+
 set RUBYLIBFILE=%RUBYLIBFILEPATH%\%RUBYLIBFILENAME%
 
 echo "    Ruby installation is in:"
@@ -66,8 +64,12 @@ echo "    - %RUBYINCLUDE2% (arch headers)"
 echo "    Ruby version code is %RUBYVERSIONCODE%"
 echo ""
 
-set QMAKE_LIBS=%LIBRARY_LIBS%\zlib.lib %RUBYLIBFILE% %PYTHONLIBFILE%
+rem Tell qmake to link zlib, Python, and Ruby libraries
+set "QMAKE_LIBS=-lz %RUBYLIBFILE% %PYTHONLIBFILE%"
 
+echo Adding extra libs:		%QMAKE_LIBS%
+
+rem Get KLayout version info
 set KLAYOUT_VERSION=%PKG_VERSION%
 
 date /t >%TEMP%\klayout-build-tmp.txt
@@ -82,10 +84,9 @@ if ERRORLEVEL 1 (
 )
 del %TEMP%\klayout-build-tmp.txt
 
-echo KLAYOUT_VERSION:		%KLAYOUT_VERSION%
+echo KLAYOUT_VERSION:		    %KLAYOUT_VERSION%
 echo KLAYOUT_VERSION_DATE:	%KLAYOUT_VERSION_DATE%
-echo KLAYOUT_VERSION_REV:	%KLAYOUT_VERSION_REV%
-
+echo KLAYOUT_VERSION_REV:	  %KLAYOUT_VERSION_REV%
 
 qmake ^
   HAVE_QT5=1 ^
@@ -119,18 +120,13 @@ qmake ^
   "RUBYINCLUDE2=%RUBYINCLUDE2%" ^
   "RUBYLIBFILE=%RUBYLIBFILE%" ^
   "QMAKE_LIBS=%QMAKE_LIBS%" ^
-  "PREFIX=%LIBRARY_BIN%" ^
-  %SRC_DIR%\klayout\src\klayout.pro || exit /b 1 
+  "PREFIX=%out_dir%" ^
+  %SRC_DIR%\klayout\src\klayout.pro || exit /b 1
 
-echo #### QMake Completed ####
+rem Build with multi-threaded nmake (jom) and install
+jom || exit /b 1
+jom install || exit /b 1
 
-jom
-
-echo #### NMake Completed ####
-
-jom install
-
-echo #### NMake Install Completed ####
-
-move %LIBRARY_BIN%\pymod\pya %SP_DIR%\pya
-move %LIBRARY_BIN%\pymod\klayout %SP_DIR%\klayout
+rem Move python modules into standard location
+move %out_dir%\pymod\pya %PREFIX%\lib\pya
+move %out_dir%\pymod\klayout %PREFIX%\lib\klayout
